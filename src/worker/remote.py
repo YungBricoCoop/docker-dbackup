11f60110f596b1ab497b4aa4511c2f8d3720e780
Elwan <elwan.mayencourt@gmail.com>
2024-10-26 23:44:27 +0200

    ♻️ Use classes to improve code quality

--- src/worker/remote.py
+++ src/worker/remote.py
@@ -1,10 +1,10 @@
 import os
-from datetime import datetime
+from abc import ABC, abstractmethod
 from ftplib import FTP, error_perm
 
 import paramiko
 from loguru import logger
-from scp import SCPClient
+from scp import SCPClient as scp_SCPClient
 
 from config import Host
 from worker.file import get_filename_from_path, get_backups_to_delete
@@ -12,21 +12,253 @@ from worker.file import get_filename_from_path, get_backups_to_delete
 DEFAULT_TIMEOUT_IN_SECONDS = 10
 
 
+class FileTransferClient(ABC):
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+    @abstractmethod
+    def transfer_file(self, local_path, remote_path):
+        pass
+
+    @abstractmethod
+    def mkdir(self, path):
+        pass
+
+    @abstractmethod
+    def chdir(self, path):
+        pass
+
+    @abstractmethod
+    def delete_file(self, path):
+        pass
+
+    @abstractmethod
+    def list_files(self, path):
+        pass
+
+
+class SCPFileTransferClient(FileTransferClient):
+    def __init__(self, host):
+        self.host = host
+        self.ssh = None
+
+    def connect(self):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if self.host.password:
+            self.ssh.connect(
+                hostname=self.host.host,
+                port=self.host.port,
+                username=self.host.username,
+                password=self.host.password,
+                timeout=DEFAULT_TIMEOUT_IN_SECONDS,
+            )
+        elif self.host.private_ssh_key:
+            self.ssh.connect(
+                hostname=self.host.host,
+                port=self.host.port,
+                username=self.host.username,
+                key_filename=self.host.private_ssh_key,
+                timeout=DEFAULT_TIMEOUT_IN_SECONDS,
+            )
+
+    def disconnect(self):
+        if self.ssh:
+            self.ssh.close()
+            self.ssh = None
+
+    def transfer_file(self, local_path, remote_path):
+        with scp_SCPClient(self.ssh.get_transport()) as scp:
+            scp.put(local_path, remote_path)
+
+    def mkdir(self, path):
+        stdin, stdout, stderr = self.ssh.exec_command(f"mkdir -p '{path}'")
+        stdout.channel.recv_exit_status()
+
+    def chdir(self, path):
+        pass
+
+    def delete_file(self, path):
+        stdin, stdout, stderr = self.ssh.exec_command(f"rm '{path}'")
+        stdout.channel.recv_exit_status()
+
+    def list_files(self, path):
+        stdin, stdout, stderr = self.ssh.exec_command(f"ls -1 '{path}'")
+        stdout.channel.recv_exit_status()
+        return stdout.read().decode().splitlines()
+
+
+# SFTP implementation
+class SFTPFileTransferClient(FileTransferClient):
+    def __init__(self, host):
+        self.host = host
+        self.ssh = None
+        self.sftp = None
+
+    def connect(self):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if self.host.password:
+            self.ssh.connect(
+                hostname=self.host.host,
+                port=self.host.port,
+                username=self.host.username,
+                password=self.host.password,
+                timeout=DEFAULT_TIMEOUT_IN_SECONDS,
+            )
+        elif self.host.private_ssh_key:
+            self.ssh.connect(
+                hostname=self.host.host,
+                port=self.host.port,
+                username=self.host.username,
+                key_filename=self.host.private_ssh_key,
+                timeout=DEFAULT_TIMEOUT_IN_SECONDS,
+            )
+
+        self.sftp = self.ssh.open_sftp()
+
+    def disconnect(self):
+        if self.sftp:
+            self.sftp.close()
+            self.sftp = None
+        if self.ssh:
+            self.ssh.close()
+            self.ssh = None
+
+    def transfer_file(self, local_path, remote_path):
+        self.sftp.put(local_path, remote_path)
+
+    def mkdir(self, path):
+        try:
+            self.sftp.mkdir(path)
+        except IOError:
+            # Directory already exists
+            pass
+
+    def chdir(self, path):
+        self.sftp.chdir(path)
+
+    def delete_file(self, path):
+        self.sftp.remove(path)
+
+    def list_files(self, path):
+        return self.sftp.listdir(path)
+
+
+# FTP implementation
+class FTPFileTransferClient(FileTransferClient):
+    def __init__(self, host):
+        self.host = host
+        self.ftp = None
+
+    def connect(self):
+        self.ftp = FTP()
+        self.ftp.connect(
+            self.host.host, self.host.port, timeout=DEFAULT_TIMEOUT_IN_SECONDS
+        )
+        self.ftp.login(user=self.host.username, passwd=self.host.password)
+
+    def disconnect(self):
+        if self.ftp:
+            self.ftp.quit()
+            self.ftp = None
+
+    def transfer_file(self, local_path, remote_path):
+        remote_dir, remote_filename = os.path.split(remote_path)
+        self.mkdir(remote_dir)
+        self.chdir(remote_dir)
+        with open(local_path, "rb") as file:
+            self.ftp.storbinary(f"STOR {remote_filename}", file)
+
+    def mkdir(self, path):
+        dirs = path.strip("/").split("/")
+        current_dir = ""
+        for dir in dirs:
+            current_dir += f"/{dir}"
+            try:
+                self.ftp.cwd(current_dir)
+            except error_perm:
+                self.ftp.mkd(current_dir)
+
+    def chdir(self, path):
+        self.ftp.cwd(path)
+
+    def delete_file(self, path):
+        self.ftp.delete(path)
+
+    def list_files(self, path):
+        self.ftp.cwd(path)
+        return self.ftp.nlst()
+
+
+class LocalFileTransferClient(FileTransferClient):
+    def __init__(self):
+        self.current_dir = os.getcwd()
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def transfer_file(self, local_path, remote_path):
+        remote_dir, remote_filename = os.path.split(remote_path)
+        self.mkdir(remote_dir)
+        os.rename(local_path, remote_path)
+
+    def mkdir(self, path):
+        os.makedirs(path, exist_ok=True)
+
+    def chdir(self, path):
+        os.chdir(path)
+        self.current_dir = path
+
+    def delete_file(self, path):
+        os.remove(path)
+
+    def list_files(self, path):
+        return os.listdir(path)
+
+
+def get_client(type: str, host=None):
+    if type == "scp":
+        return SCPFileTransferClient(host)
+    elif type == "sftp":
+        return SFTPFileTransferClient(host)
+    elif type == "ftp":
+        return FTPFileTransferClient(host)
+    elif type == "local":
+        return LocalFileTransferClient()
+    else:
+        raise ValueError(f"Unknown type: {type}")
+
+
 def remote_transfer(
     type: str,
     local_filepath: str,
     remote_dir_path: str,
-    host: Host,
+    host,
 ):
-    remote_filename = get_filename_from_path(local_filepath)
-    if type == "scp":
-        send_file_over_ssh_with_scp(
-            local_filepath, remote_dir_path, remote_filename, host
-        )
-    elif type == "sftp":
-        send_file_over_sftp(local_filepath, remote_dir_path, remote_filename, host)
-    elif type == "ftp":
-        send_file_over_ftp(local_filepath, remote_dir_path, remote_filename, host)
+    client = get_client(type, host)
+    client.connect()
+    try:
+        client.mkdir(remote_dir_path)
+        remote_filename = get_filename_from_path(local_filepath)
+        remote_filepath = os.path.join(remote_dir_path, remote_filename)
+        client.transfer_file(local_filepath, remote_filepath)
+        logger.info(f"File sent successfully to {remote_filepath}")
+    except Exception as e:
+        logger.error(f"Failed to send file: {e}")
+        raise
+    finally:
+        client.disconnect()
 
 
 def remote_remove_old_backups(
@@ -35,321 +267,23 @@ def remote_remove_old_backups(
     filename_prefix: str,
     date_format: str,
     max_backup_files: int,
-    host: Host,
+    host,
 ):
-    if type == "scp":
-        remove_old_backups_scp(
-            remote_dir_path, filename_prefix, date_format, max_backup_files, host
-        )
-    elif type == "sftp":
-        remove_old_backups_sftp(
-            remote_dir_path, filename_prefix, date_format, max_backup_files, host
-        )
-    elif type == "ftp":
-        remove_old_backups_ftp(
-            remote_dir_path, filename_prefix, date_format, max_backup_files, host
-        )
-
-
-def get_ssh_client(host: Host):
-    """
-    Creates an SSH client with the provided host configuration.
-    """
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    if host.password:
-        ssh.connect(
-            hostname=host.host,
-            port=host.port,
-            username=host.username,
-            password=host.password,
-            timeout=DEFAULT_TIMEOUT_IN_SECONDS,
-        )
-    elif host.private_ssh_key:
-        ssh.connect(
-            hostname=host.host,
-            port=host.port,
-            username=host.username,
-            key_filename=host.private_ssh_key,
-            timeout=DEFAULT_TIMEOUT_IN_SECONDS,
-        )
-
-    return ssh
-
-
-def get_ftp_client(host: Host):
-    """
-    Creates an FTP client with the provided host configuration.
-    """
-    ftp = FTP()
-    ftp.connect(host.host, host.port, timeout=DEFAULT_TIMEOUT_IN_SECONDS)
-    ftp.login(user=host.username, passwd=host.password)
-    return ftp
-
-
-def send_file_over_ssh_with_scp(
-    local_filepath: str, remote_dir_path: str, remote_filename: str, host: Host
-):
-    """
-    Sends a file over SSH to a remote host using SCP.
-
-    :param local_filepath: Path to the local file to send.
-    :param remote_dir_path: Destination directory path on the remote host.
-    :param remote_filename: Name of the file on the remote host.
-    :param host: Host configuration including hostname, port, username, password, or private SSH key.
-    """
-
-    remote_filepath = os.path.join(remote_dir_path, remote_filename)
-    remote_dest = f"{host.username}@{host.host}:{remote_filepath}"
-    logger.info(f"Sending file over SSH using SCP: {local_filepath} -> {remote_dest}")
-
+    client = get_client(type, host)
+    client.connect()
     try:
-        ssh = get_ssh_client(host)
-
-        stdin, stdout, stderr = ssh.exec_command(f"mkdir -p '{remote_dir_path}'")
-        stdout.channel.recv_exit_status()
-
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.put(local_filepath, remote_filepath)
-
-        logger.info(f"File sent successfully over SSH to {remote_dest}")
+        files = client.list_files(remote_dir_path)
+        files_to_delete = get_backups_to_delete(
+            files, filename_prefix, date_format, max_backup_files
+        )
+        if not files_to_delete:
+            return
+        for file in files_to_delete:
+            remote_file_path = os.path.join(remote_dir_path, file)
+            client.delete_file(remote_file_path)
+            logger.info(f"Deleted old backup file: {remote_file_path}")
     except Exception as e:
-        logger.error(f"Failed to send file over SSH using SCP: {e}")
+        logger.error(f"Failed to remove old backups: {e}")
         raise
     finally:
-        ssh.close()
-
-
-def send_file_over_sftp(
-    local_filepath: str, remote_dir_path: str, remote_filename: str, host: Host
-):
-    """
-    Sends a file over SFTP to a remote host.
-
-    :param local_filepath: Path to the local file to send.
-    :param remote_dir_path: Destination directory path on the remote host.
-    :param remote_filename: Name of the file on the remote host.
-    :param host: Host configuration including hostname, port, username, password, or private SSH key.
-    """
-
-    remote_filepath = os.path.join(remote_dir_path, remote_filename)
-    remote_dest = f"{host.username}@{host.host}:{remote_filepath}"
-    logger.info(f"Sending file over SFTP: {local_filepath} -> {remote_dest}")
-
-    try:
-        ssh = get_ssh_client(host)
-
-        sftp = ssh.open_sftp()
-
-        try:
-            sftp.chdir(remote_dir_path)
-        except IOError:
-            dir_paths = remote_dir_path.strip("/").split("/")
-            current_dir = ""
-            for dir in dir_paths:
-                current_dir += f"/{dir}"
-                try:
-                    sftp.mkdir(current_dir)
-                except IOError:
-                    pass
-                sftp.chdir(current_dir)
-
-        sftp.put(local_filepath, remote_filepath)
-
-        sftp.close()
-        ssh.close()
-
-        logger.info(f"File sent successfully over SFTP to {remote_dest}")
-    except Exception as e:
-        logger.error(f"Failed to send file over SFTP: {e}, {remote_dest}")
-        raise
-
-
-def create_ftp_directory_tree(ftp, remote_dir):
-    """
-    Creates directories recursively on the FTP server.
-
-    :param ftp: Active FTP connection.
-    :param remote_dir: Remote directory path to create.
-    """
-    dirs = remote_dir.strip("/").split("/")
-    current_dir = ""
-    for dir in dirs:
-        current_dir += f"/{dir}"
-        try:
-            ftp.cwd(current_dir)
-        except error_perm:
-            ftp.mkd(current_dir)
-
-
-def send_file_over_ftp(
-    local_filepath: str, remote_dir_path: str, remote_filename: str, host: Host
-):
-    """
-    Sends a file over FTP to a remote server.
-
-    :param local_filepath: Path to the local file to send.
-    :param remote_dir_path: Destination directory path on the remote server.
-    :param remote_filename: Name of the file on the remote server.
-    :param host: Host configuration including hostname, port, username, and password.
-    """
-
-    remote_filepath = os.path.join(remote_dir_path, remote_filename)
-    remote_dest = f"{host.username}@{host.host}:{remote_filepath}"
-    logger.info(f"Sending file over FTP: {local_filepath} -> {remote_dest}")
-
-    try:
-        ftp = get_ftp_client(host)
-
-        if remote_dir_path:
-            try:
-                ftp.cwd(remote_dir_path)
-            except error_perm:
-                create_ftp_directory_tree(ftp, remote_dir_path)
-                ftp.cwd(remote_dir_path)
-
-        with open(local_filepath, "rb") as file:
-            ftp.storbinary(f"STOR {remote_filename}", file)
-
-        ftp.quit()
-
-        logger.info(f"File sent successfully over FTP to {remote_dest}")
-    except Exception as e:
-        logger.error(f"Failed to send file over FTP: {e}, {remote_dest}")
-        raise
-
-
-def remove_old_backups_scp(
-    remote_dir_path: str,
-    filename_prefix: str,
-    date_format: str,
-    max_backup_files: int,
-    host: Host,
-):
-    """
-    Removes old backup files over SSH (SCP) that exceed the max_backup_files limit.
-
-    :param remote_dir_path: Remote directory path where backups are stored.
-    :param filename_prefix: Prefix of the backup filenames to identify relevant files.
-    :param date_format: Date format used in the backup filenames.
-    :param max_backup_files: Maximum number of backup files to keep.
-    :param host: Host configuration including hostname, port, username, password, or private SSH key.
-    """
-
-    try:
-        ssh = get_ssh_client(host)
-
-        stdin, stdout, stderr = ssh.exec_command(f"ls -1 {remote_dir_path}")
-        files = stdout.read().decode().splitlines()
-
-        files_to_delete = get_backups_to_delete(
-            files, filename_prefix, date_format, max_backup_files
-        )
-
-        if len(files_to_delete) == 0:
-            ssh.close()
-            return
-        for file in files_to_delete:
-            remote_file_path = os.path.join(remote_dir_path, file)
-            print(remote_file_path)
-            try:
-                ssh.exec_command(f"rm '{remote_file_path}'")
-                logger.info(f"Deleted old backup file: {remote_file_path}")
-            except Exception as e:
-                logger.error(f"Failed to delete file '{remote_file_path}': {e}")
-
-        ssh.close()
-    except Exception as e:
-        logger.error(f"Failed to remove old backups over SSH: {e}")
-        raise
-
-
-def remove_old_backups_sftp(
-    remote_dir_path: str,
-    filename_prefix: str,
-    date_format: str,
-    max_backup_files: int,
-    host: Host,
-):
-    """
-    Removes old backup files over SFTP that exceed the max_backup_files limit.
-
-    :param remote_dir_path: Remote directory path where backups are stored.
-    :param filename_prefix: Prefix of the backup filenames to identify relevant files.
-    :param date_format: Date format used in the backup filenames.
-    :param max_backup_files: Maximum number of backup files to keep.
-    :param host: Host configuration including hostname, port, username, password, or private SSH key.
-    """
-
-    try:
-        ssh = get_ssh_client(host)
-
-        sftp = ssh.open_sftp()
-        sftp.chdir(remote_dir_path)
-        files = sftp.listdir()
-
-        files_to_delete = get_backups_to_delete(
-            files, filename_prefix, date_format, max_backup_files
-        )
-        if len(files_to_delete) == 0:
-            sftp.close()
-            ssh.close()
-            return
-
-        for file in files_to_delete:
-            remote_file_path = os.path.join(remote_dir_path, file)
-            try:
-                sftp.remove(remote_file_path)
-                logger.info(f"Deleted old backup file: {remote_file_path}")
-            except Exception as e:
-                logger.error(f"Failed to delete file '{remote_file_path}': {e}")
-
-        sftp.close()
-        ssh.close()
-    except Exception as e:
-        logger.error(f"Failed to remove old backups over SFTP: {e}")
-        raise
-
-
-def remove_old_backups_ftp(
-    remote_dir_path: str,
-    filename_prefix: str,
-    date_format: str,
-    max_backup_files: int,
-    host: Host,
-):
-    """
-    Removes old backup files over FTP that exceed the max_backup_files limit.
-
-    :param remote_dir_path: Remote directory path where backups are stored.
-    :param filename_prefix: Prefix of the backup filenames to identify relevant files.
-    :param date_format: Date format used in the backup filenames.
-    :param max_backup_files: Maximum number of backup files to keep.
-    :param host: Host configuration including hostname, port, username, and password.
-    """
-
-    try:
-        ftp = get_ftp_client(host)
-        ftp.cwd(remote_dir_path)
-        files = ftp.nlst()
-
-        files_to_delete = get_backups_to_delete(
-            files, filename_prefix, date_format, max_backup_files
-        )
-
-        if len(files_to_delete) == 0:
-            ftp.quit()
-            return
-
-        for file in files_to_delete:
-            try:
-                ftp.delete(file)
-                logger.info(f"Deleted old backup file: {file}")
-            except Exception as e:
-                logger.error(f"Failed to delete file '{file}': {e}")
-
-        ftp.quit()
-    except Exception as e:
-        logger.error(f"Failed to remove old backups over FTP: {e}")
-        raise
+        client.disconnect()
